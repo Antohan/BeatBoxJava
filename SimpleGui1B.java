6ee3a597e39262a6865c6f6c3e0fcb3f24abb335
Antohan <justanton19@gmail.com>
2017-03-01 09:33:54 +0700

    Add simpe action in GUI

---
+++ SimpleGui1B.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class SimpleGui1B implements ActionListener {
+
+	JButton button;
+	JFrame frame;
+
+	public static void main(String[] args) {
+		SimpleGui1B gui = new SimpleGui1B();
+		gui.go();
+	}
+
+	public void go() {
+		frame = new JFrame();
+		button = new JButton("click me");
+
+		button.addActionListener(this);
+
+		frame.getContentPane().add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		button.setText("I've been clicked!");
+	}
+}
